a84e59d904585518e010b7676a13cfa1d54aefde
thanhminhspk05 <69253365+thanhminhspk05@users.noreply.github.com>
2024-01-24 15:25:39 +0700

    finish lesson 45.1

--- src/main/java/com/project/shopapp/ShopappApplication.java
+++ src/main/java/com/project/shopapp/ShopappApplication.java
@@ -2,12 +2,12 @@ package com.project.shopapp;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@SecurityScheme(name = "javainuseapi", scheme = "basic", type= SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class ShopappApplication {
 
 	public static void main(String[] args) {
